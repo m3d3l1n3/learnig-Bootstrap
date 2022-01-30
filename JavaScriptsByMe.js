430468294850ba70087056262ac9c814a7cdb3b4
m3d3l1n3 <82612183+m3d3l1n3@users.noreply.github.com>
2022-01-30 15:11:27 +0200

    changing colors by button

--- JavaScriptsByMe.js
+++ JavaScriptsByMe.js
@@ -1,6 +1,32 @@
+// Functia asigura miscarea instanta la inceputul paginii (butonul verde)
 function ScrolTop() {
-    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+    var place = { top: 0, left: 0, behavior: 'instant' };
+    window.scrollTo(place);
 }
-function Run(){
-    
+
+// asigura schimbarea in mod periodic a culorii, in fct de schema aleasa + alegerea schemei
+let colors = [["HotPink", "SeaGreen", "RebeccaPurple", "Aquamarine", "FireBrick"], ["BlanchedAlmond", "Lavender", "LemonChiffon", "LavenderBlush"], ["Chocolate", "LightCoral", "Peru"]];
+let text_color = ["white", "black", "grey"];
+let tom = -1;
+let jerry = 0;
+let timer;
+function ChangingColors() {
+    tom++;
+    if (timer) clearInterval(timer);
+    timer = setInterval(ChangingBackground, 1000);
+    if (colors.length <= tom) tom = 0;
+}
+// schimba intrele ele culorile dintr-o schema 
+function ChangingBackground() {
+    document.body.style.backgroundColor = colors[tom][jerry];
+    document.body.style.color = text_color[tom]; // schimba culoarea textul in fct de schema (pt vizibilitate)
+    let tables = document.getElementsByClassName("table");
+    // schimba culoarea textul in fct de schema (la tabele)
+    for (let element of tables) {
+        element.style.color = text_color[tom];
+    }
+    console.log("tom- ", tom);
+    console.log("jerry- ", jerry);
+    jerry++;
+    if (colors[tom].length <= jerry) jerry = 0;
 }
